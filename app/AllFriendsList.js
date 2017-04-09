362f6df06969499af4371333e389b043b5e8d058
Tiffany <tiffanylin27@gmail.com>
2017-04-09 16:00:58 -0700

    Implement local server api testing functionality

--- app/AllFriendsList.js
+++ app/AllFriendsList.js
@@ -1,10 +1,23 @@
 import React, { Component } from 'react';
 import { View, Text, StyleSheet } from 'react-native';
+import endpoint from './endpoint.js';
 
 export default class AllFriendsList extends Component {
   constructor(props) {
     super(props);
     this.state =  {};
+    fetch(endpoint + '/api/test')
+      .then(function(response) {
+        return response.json();
+      })
+      .then(function(data) {
+        console.log('data: ', data);
+        this.setState(data);
+      }.bind(this))
+      .catch(function(error) {
+        console.log('There was an error in fetching your data: ', error);
+        return error;
+      });
   }
 
   static navigationOptions = {
@@ -16,6 +29,7 @@ export default class AllFriendsList extends Component {
     return (
       <View>
         <Text>Contacts List</Text>
+        <Text>{ this.state.name }</Text>
       </View>
     );
   }
---
+++ app/endpoint.example.js
@@ -0,0 +1,31 @@
+/*
+
+INSTRUCTIONS TO GET A LOCAL TEST SERVER WORKING WITH YOUR APP
+
+1. Make sure that your USB connected phone and laptop are on the same wifi network
+  - I haven't tested or tried to get this working on the emulator--someone is welcome to add directions to this as needed 
+
+2. Run your server via the nodemon command from the root/server folder in the safer-server repo
+
+3. Open up System Preferences > Network 
+
+4. Observe the IP address (should look like this)
+  - Wi-Fi is connected to Hack Reactor and has the IP address xx.x.xx.xxx.
+
+5. Open up the developer menu while the app is running (shake the phone)
+
+6. Select Dev Settings > Debug server host & port for device
+
+7. Type in the IP address you found from Step 3, WITH PORT OF 8081
+  - Example: 10.6.64.195:8081
+
+8. Now, replace the endpoint variable here with your IP address using the port we designated - 1337
+
+9. Whenever you make a fetch request to our server, make sure you're using the endpoint here + whatever route you want
+
+*/
+
+// Please fill and rename this file to endpoint.js
+// endpoint.js is included in the gitignore
+//export default endpoint = 'http://<your IP address here>:1337'
+
