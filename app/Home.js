9ce5d733f7701508f1625150b6e939aee0fe41e1
Kyle Bradford <kfoxb@kfoxb.com>
2017-04-11 18:23:17 -0700

    add conditional render of Google Login button

--- app/Home.js
+++ app/Home.js
@@ -8,40 +8,85 @@ import {GoogleSignin, GoogleSigninButton} from 'react-native-google-signin';
 export default class HomeScreen extends Component {
   constructor(props) {
     super(props);
-    this.state = {};
+    this.state = {
+      user: null
+    };
   };
 
   static navigationOptions = {
     title: 'Favorites'
   };
 
+  componentDidMount() {
+    this._setupGoogleSignin();
+  };
+
   render() {
     const { navigate } = this.props.navigation;
-    return (
-      <View style={styles.container}>
-        <Text>Notifications???</Text>
-        <Button
-          onPress={() => navigate('FriendMap', { 
-              friendId: 1234567890,
-              friendName: 'Kyle' 
-            })
-          }
-          title="See yo friend yo"
-        />
-        <Button 
-          onPress ={() => navigate('AddFence')}
-          title="Geofence"
-        />
-        <GoogleSigninButton
-          style={{width: 312, height: 48}}
-          size={GoogleSigninButton.Size.Wide}
-          color={GoogleSigninButton.Color.Dark}
-          //onPress={this._signIn.bind(this)}
-        />
-      </View>
-    );
-  };
-};
+    if (!this.state.user) {
+      return (
+        <View style={styles.container}>
+          <GoogleSigninButton style={{width: 120, height: 44}} color={GoogleSigninButton.Color.Light} size={GoogleSigninButton.Size.Icon} onPress={() => { this._signIn(); }}/>
+        </View>
+      );
+    } else {
+      return (
+        <View style={styles.container}>
+          <Text>Hello {this.state.user.name}</Text>
+          <Text>Notifications???</Text>
+          <Button
+            onPress={() => navigate('FriendMap', { 
+                friendId: 1234567890,
+                friendName: 'Kyle' 
+              })
+            }
+            title="See yo friend yo"
+          />
+          <Button 
+            onPress ={() => navigate('AddFence')}
+            title="Geofence"
+          />
+        </View>
+      )
+    }
+  }
+
+  async _setupGoogleSignin() {
+    try {
+      await GoogleSignin.hasPlayServices({ autoResolve: true });
+      await GoogleSignin.configure({
+        webClientId: '603421766430-60og8n04mebic8hi49u1mrcmcdmugnd5.apps.googleusercontent.com',
+        offlineAccess: false
+      });
+
+      const user = await GoogleSignin.currentUserAsync();
+      console.log(user);
+      this.setState({user});
+    }
+    catch(err) {
+      console.log("Play services error", err.code, err.message);
+    }
+  }
+
+  _signIn() {
+    GoogleSignin.signIn()
+    .then((user) => {
+      console.log(user);
+      this.setState({user: user});
+    })
+    .catch((err) => {
+      console.log('WRONG SIGNIN', err);
+    })
+    .done();
+  }
+
+  _signOut() {
+    GoogleSignin.revokeAccess().then(() => GoogleSignin.signOut()).then(() => {
+      this.setState({user: null});
+    })
+    .done();
+  }
+}
 
 const styles = StyleSheet.create({
   container: {
