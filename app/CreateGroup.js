c6c12e2c865cda0af3624b3b085308922b09bef8
RDF014 <rdf014@gmail.com>
2017-04-13 14:42:40 -0700

    fixed styling for checkboxes

--- app/CreateGroup.js
+++ app/CreateGroup.js
@@ -55,6 +55,7 @@ export default class CreateGroup extends Component {
           renderRow={(rowData) => (
             <CheckBox
               label={rowData.first}
+              underlayColor='none'
             />
           )}
         />
