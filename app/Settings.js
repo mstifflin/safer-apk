3121c2e69826c6fd6c017b4034f7f6f28f315228
Tiffany <tiffanylin27@gmail.com>
2017-04-14 16:48:57 -0700

    Implement incognito mode settings

--- app/Settings.js
+++ app/Settings.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { View, Button, Text, Picker, StyleSheet } from 'react-native';
+import { View, Button, Text, Picker, Switch, StyleSheet } from 'react-native';
 import { endpoint } from './endpoint.js';
 
 import FriendMap from './FriendMap.js';
@@ -13,16 +13,18 @@ export default class Settings extends Component {
     this.state = {
       userId: 1, 
       selected: 'GPS', //TODO: set this to the privacy setting grabbed from the sql database
+      incognito: false,
       error: false
     };
-    this.onValueChange = this.onValueChange.bind(this);
+    this.pickerChange = this.pickerChange.bind(this);
+    this.switchChange = this.switchChange.bind(this);
   };
 
   static navigationOptions = {
     title: 'Settings'
   };
 
-  onValueChange(privacySetting) {
+  pickerChange(privacySetting) {
     fetch(endpoint + '/api/privacySettings', {
       method: 'PUT',
       headers: {
@@ -45,26 +47,65 @@ export default class Settings extends Component {
         error: true
       });
     });
+
     this.setState({
       selected: privacySetting,
     });
+  }
+
+  switchChange(value) {
+    console.log('value in switch change: ', value);
+    fetch(endpoint + '/api/privacySettings', {
+      method: 'PUT',
+      headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        userId: this.state.userId,
+        incognito: value
+      })
+
+    }).then((response) => {
+      if (response.err) {
+        this.setState({
+          error: true
+        });
+      }
+    }).catch((err) => {
+      this.setState({
+        error: true
+      });
+    });
+
+    this.setState({
+      incognito: !this.state.incognito
+    })
   }
 
   render() {
     const { navigate } = this.props.navigation;
     return (
       <View style={styles.container}>
+
         <Text>Privacy Settings</Text>
-        {this.state.error && 
-          (<Text>There was an error updating your privacy settings. Please try again later.</Text>)}
         <Picker 
           style={{width: 200}} 
           selectedValue={this.state.selected}
-          onValueChange={this.onValueChange} >
+          onValueChange={this.pickerChange} >
           <Picker.Item label='Label' value='label'/>
           <Picker.Item label='GPS' value='gps' />
         </Picker>
 
+        <Text>Incognito Mode</Text>
+        <Switch
+          onValueChange={this.switchChange}
+          style={{marginBottom: 10}}
+          value={this.state.incognito} />
+
+        {this.state.error &&
+          (<Text>There was an error updating your privacy settings. Please try again later.</Text>)}
+          
       </View>
     )
   }
