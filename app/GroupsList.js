14dc1283a19f9e01fb44083d1e2dcc6098f3103d
RDF014 <rdf014@gmail.com>
2017-04-14 14:53:03 -0700

    working on rendering all group names

--- app/GroupsList.js
+++ app/GroupsList.js
@@ -1,10 +1,27 @@
 import React, { Component } from 'react';
+import { endpoint } from './endpoint.js';
 import { Button, View, Text, StyleSheet } from 'react-native';
 
 export default class GroupsList extends Component {
   constructor(props) {
     super(props);
     this.state =  {};
+  }
+
+  componentWillMount () {
+    console.log('getgroups')
+    fetch(`${endpoint}/api/groups`, {
+      method: 'GET',
+    })
+    .then(res => {
+      return res.json();
+    })
+    .then(groups => {
+      console.log(groups);
+    })
+    .catch(err => {
+      console.log('There was error in getting groups', err);
+    })
   }
 
   static navigationOptions = {
