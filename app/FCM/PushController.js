c784436e00f3ade21f22ea5103bc9f3d64e0aa70
Tiffany <tiffanylin27@gmail.com>
2017-04-15 00:12:04 -0700

    Implement sending a FCM token to store in DB upon app load

--- app/FCM/PushController.js
+++ app/FCM/PushController.js
@@ -1,11 +1,16 @@
 import React, { Component } from "react";
 import { Platform } from 'react-native';
 import FCM, {FCMEvent, RemoteNotificationResult, WillPresentNotificationResult, NotificationType} from "react-native-fcm";
-import { firebaseClient } from  "../endpoint.js";
+import { endpoint, firebaseClient } from  "../endpoint.js";
 
 export default class PushController extends Component {
   constructor(props) {
     super(props);
+    //TODO: get user id from sql server upon login
+    //and pass it along with props
+    this.state = {
+      userId: 1
+    }
   };
 
   componentDidMount() {
@@ -13,9 +18,25 @@ export default class PushController extends Component {
 
     FCM.getFCMToken()
     .then( token => {
-      console.log('Token in getFCMToken in PushController.js: ', token);
       this.props.onChangeToken(token);
+
+      return fetch(`${endpoint}/api/fcmToken`, {
+        method: 'POST',
+        headers: {
+          'Accept': 'application/json',
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+          userId: this.state.userId,
+          FCMToken: token
+        })
+      });
+    }).then(response => {
+      console.log('response in pushcontroller: ', response);
+    }).catch(err => {
+      console.log(err);
     });
+
 
     FCM.getInitialNotification()
     .then( notif => {
