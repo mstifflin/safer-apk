bbc772b9d6134237882285064450cb4b90637277
Tiffany <tiffanylin27@gmail.com>
2017-04-21 11:00:51 -0700

    Refactor to use AuthAxios

--- app/FCM/PushController.js
+++ app/FCM/PushController.js
@@ -2,52 +2,39 @@ import React, { Component } from "react";
 import { Platform } from 'react-native';
 import FCM, {FCMEvent, RemoteNotificationResult, WillPresentNotificationResult, NotificationType} from "react-native-fcm";
 import { endpoint, firebaseClient } from  "../endpoint.js";
+import AuthAxios from './AuthAxios.js';
 
 export default class PushController extends Component {
   constructor(props) {
     super(props);
-    //TODO: get user id from sql server upon login
-    //and pass it along with props
-    this.state = {
-      userId: 1
-    }
   };
 
   componentDidMount() {
-    // FCM.requestPermissions(); //iOS only
-
     FCM.getFCMToken()
-    .then( token => {
+    .then( (token) => {
       this.props.onChangeToken(token);
 
-      return fetch(`${endpoint}/api/fcmToken`, {
-        method: 'POST',
-        headers: {
-          'Accept': 'application/json',
-          'Content-Type': 'application/json',
-        },
-        body: JSON.stringify({
-          userId: this.state.userId,
-          FCMToken: token
-        })
-      });
-    }).then(response => {
+      return AuthAxios({
+        url: '/user',
+        method: 'put',
+        data: {FCMToken: token}
+      })
     }).catch(err => {
       console.error(err);
     });
 
     FCM.getInitialNotification()
-    .then( notif => {
+    .then((notif) => {
       // console.log('initial notification: ', notif);
     });
 
-    this.notificationListener = FCM.on(FCMEvent.Notification, notif => {
+    this.notificationListener = FCM.on(FCMEvent.Notification, (notif) => {
       if (notif.local_notification) { return; }
       if (notif.opened_from_tray) { return; }
       this.showLocalNotification(notif);
     });
 
-    this.refreshTokenListener = FCM.on(FCMEvent.RefreshToken, token => {
+    this.refreshTokenListener = FCM.on(FCMEvent.RefreshToken, (token) => {
       console.log('token (refreshUnsubcribe)', token);
       this.props.onChangeToken(token);
     })
