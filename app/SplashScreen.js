6b2e04927c8dad6ad5b461e71c8ddb4491f35507
Tiffany <tiffanylin27@gmail.com>
2017-04-23 20:09:03 -0700

    Move geolocation to SplashScreen, refactor to make dryer and... move some parsing logic to the server

--- app/SplashScreen.js
+++ app/SplashScreen.js
@@ -13,8 +13,6 @@ export default class SplashScreen extends Component {
       user: null,
       initialPosition: 'unknown',
       lastPosition: 'unknown',
-      phoneNumber: '',
-      currentlyAt: ''
     };
   };
 
@@ -22,60 +20,38 @@ export default class SplashScreen extends Component {
     title: 'Welcome'
   };
   
+  componentDidMount() {
+    this._setupGoogleSignin();
+    this.geoMonitoring();
+  };
+
+  componentWillUnmount() {
+    navigator.geolocation.clearWatch(this.watchID);
+  }
+
   watchID: ?number = null;
 
   geoMonitoring() {
     navigator.geolocation.getCurrentPosition(
       (position) => {
-        var initialPosition = JSON.stringify(position);
-        this.setState({initialPosition});
+        console.log('got the position!', position);
+        this.setState({initialPosition: position});
       },
       (error) => alert(`We couldn't get your location`),
       {enableHighAccuracy: false, timeout: 20000, maximumAge: 90000000000000}
     );
     this.watchID = navigator.geolocation.watchPosition((position) => {
-      var lastPosition = JSON.stringify(position);
-      this.setState({lastPosition});
-
-      let point = {
-        lat: position.coords.latitude,
-        lng: position.coords.longitude,
-      };
+      this.setState({lastPosition: position});
     });
   }
 
-  // TODO: check fences once confirmed that user is logged in (this function calls saveLocation)
-  checkFences(currentPoint) {
-    let phoneNumber = '1234567'
-    AuthAxios({
-      url: `/api/labels/?id=${phoneNumber}`
-    })
-    .then(({data}) => {
-      let fences = data;
-      for (let fence of fences) {
-        let proximity = distanceBetweenCoordinates(currentPoint.lat, currentPoint.lng, fence.lat, fence.lng);
-        const radius = 0.5;
-        if (proximity < radius) {
-          this.setState({currentlyAt: fence.label}, () => this.saveLocation());
-          break;  
-        } else {
-          this.setState({currentlyAt: 'Elsewhere'}, () => this.saveLocation());
-        }
-      }
-    })
-    .catch((error) => {
-      alert(`Oh no, we couldn't get your fences!`)
-    })
-  }
-
   saveLocation () {
-    console.log('Save location called', this.state.currentlyAt)
-    let location = {};
-    let position = JSON.parse(this.state.lastPosition);
-    location.lat = position.coords.latitude;
-    location.lng = position.coords.longitude;
-    location.label = this.state.currentlyAt;
-    location.user = '1234567'
+    console.log('Save location called from splash screen')
+    let position = this.state.initialPosition;
+    let location = {
+      lat: position.coords.latitude,
+      long: position.coords.longitude
+    };
 
     AuthAxios({
       url: `/api/users/location`,
@@ -86,34 +62,10 @@ export default class SplashScreen extends Component {
     .catch(error => alert('Location not updated'))
   }
 
-  componentDidMount() {
-    this._setupGoogleSignin();
-  };
-
-  componentWillUnmount() {
-    navigator.geolocation.clearWatch(this.watchID);
-  }
-
-  // if (this.state.user) {
-  //   return (
-  //     <View>
-  //       <PushController />
-  //       <HomeFavorite />
-  //     </View>
-  //   )
-  // }
-
-
-  render() {
-    const { navigate } = this.props.navigation;
-    return (
-      <Text>Safer SplashScreen</Text> 
-    );
-  }
-
   async _setupGoogleSignin() {
     const { navigate } = this.props.navigation;
     try {
+      console.log('in setup google sign in');
       await GoogleSignin.hasPlayServices({ autoResolve: true });
       await GoogleSignin.configure({
         webClientId: googleAuthWebClientId, //replace with client_id from google-services.json
@@ -122,8 +74,9 @@ export default class SplashScreen extends Component {
 
       const user = await GoogleSignin.currentUserAsync();
       if (user === null) {//TODO: set up conditional navigation here based on whether use is null
-        navigate('SignUp');
+        navigate('SignUp', {location: this.state.initialPosition});
       } else {
+        saveLocation();
         navigate('HomePageTabs');
       }
       this.setState({user});
@@ -131,6 +84,13 @@ export default class SplashScreen extends Component {
     catch(err) {
       console.log("Play services error", err.code, err.message);
     }
+  }
+
+  render() {
+    const { navigate } = this.props.navigation;
+    return (
+      <Text>Safer SplashScreen</Text> 
+    );
   }
 
 }
